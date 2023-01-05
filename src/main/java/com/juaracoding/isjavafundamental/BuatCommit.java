cf6b10397cda09aa9251fd31908eb3235599a606
Inten1 <intensumunar897@gmail.com>
2023-01-05 16:54:35 +0700

    Java Fundamental on Intelij commit process

--- src/main/java/com/juaracoding/isjavafundamental/BuatCommit.java
+++ src/main/java/com/juaracoding/isjavafundamental/BuatCommit.java
@@ -1,7 +1,14 @@
 package com.juaracoding.isjavafundamental;
 
 public class BuatCommit {
+    /*
+    psvm   = main() method declaration
+    sout   = prints a string to System.out
+    ctrl+D = copy
+     */
     public static void main(String[] args) {
+        System.out.println("BUAT COMMIT KE GITHUB");
         System.out.println("BUAT COMMIT KE GITHUB");
     }
 }
+
---
+++ src/main/java/com/juaracoding/isjavafundamental/CobaClass.java
@@ -0,0 +1,16 @@
+package com.juaracoding.isjavafundamental;
+/*
+Created By IntelliJ IDEA 2022.3.1 (Community Edition)
+Build #IC-223.8214.52, built on December 20, 2022
+@Author dell a.k.a. Inten Sumunar
+Java Developer
+Created on 05/01/2023 16:23
+@Last Modified 05/01/2023 16:23
+Version 1.1
+*/
+
+public class CobaClass {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
