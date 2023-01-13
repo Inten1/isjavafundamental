735a6699b307200ccd02abe538b002ac0be30ab1
Inten1 <intensumunar897@gmail.com>
2023-01-13 19:08:58 +0700

    Java Fundamental on Intelij

---
+++ src/main/java/com/juaracoding/isjavafundamental/testharian/TestHarian.java
@@ -0,0 +1,16 @@
+package com.juaracoding.isjavafundamental.testharian;
+/*
+Created By IntelliJ IDEA 2022.3.1 (Community Edition)
+Build #IC-223.8214.52, built on December 20, 2022
+@Author dell a.k.a. Inten Sumunar
+Java Developer
+Created on 13/01/2023 19:05
+@Last Modified 13/01/2023 19:05
+Version 1.1
+*/
+
+public class TestHarian {
+    public static void main(String[] args) {
+        System.out.println("COBA COBA");
+    }
+}
